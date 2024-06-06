1e3a14e5f8e29a56df37643468fc7e6269495ab1
Dhairya Patel <dpvp1403@gmail.com>
2024-06-06 21:58:41 +0530

    flask

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from flask import Flask, render_template
 import platform
-import streamlit as st
-st.write(platform.node())
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_node_name():
+    node_name = platform.node()
+    return render_template('index.html', node_name=node_name)
+
+if __name__ == '__main__': 
+    app.run(debug=True)
