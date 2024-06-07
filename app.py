869ef46bb7e2b6811954c5e6c700160e1186b9fe
Dhairya Patel <dpvp1403@gmail.com>
2024-06-07 11:12:41 +0530

    hosting vercel public ip

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
-import platform
+import requests
 
 app = Flask(__name__)
 
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        return response.json().get('ip')
+    except requests.RequestException:
+        return 'Unable to retrieve IP address'
+
 @app.route('/')
 def get_node_name():
-    node_name = platform.node()
-    return render_template('index.html', node_name=node_name)
-
+    public_ip = get_public_ip()
+    return render_template('index.html', node_name=public_ip)
